9ae1199e650bbcb5bb76f2970c1a479744f96d94
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-11 08:57:27 -0500

    done with assignment

--- javascript/graph_dfs.js
+++ javascript/graph_dfs.js
@@ -1,12 +1,27 @@
-function isPath(graph, vertexA, vertexB) {
-  // type your code here
+function isPath(graph, vertexA, vertexB, visited = new Set()) {
+  if (graph[vertexA].length === 0) {
+		return false;
+	}
+
+	visited.add(vertexA);
+
+	if (graph[vertexA].includes(vertexB)) {
+		return true;
+	} else {
+		// return !!graph[vertexA].find((vertex) => isPath(graph, vertex, vertexB));
+		return !!graph[vertexA].find((vertex) => {
+			if (!visited.has(vertex)) {
+				return isPath(graph, vertex, vertexB, visited);
+			}
+		});
+	}
 }
 
 if (require.main === module) {
   // add your own tests in here
   let graph = {
     jan: ["john", "jambaby"],
-    john: ["carl"],
+    john: ["jan", "carl"],
     jambaby: [],
     carl: ["jambaby"],
     dave: []
@@ -25,3 +40,9 @@ module.exports = isPath;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/**
+ * Check if A's adjacency list includes B
+ * If yes, return true
+ * If not, for each element in A's adjacency list, check if element's adjacency list includes B
+ * If element's adjacency list is empty, return falsey value
+ */
